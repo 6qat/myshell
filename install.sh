dec91584da015e0bf4ce09c26ea127220269fc39
Guilherme Moreira <ceschiatti@gmail.com>
2020-06-21 21:11:30 -0300

    Add more tweaks

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
 
+sudo apt install zsh -y
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
