31430c11ca0c8ddbdc80163aef957dbd3877e852
Guilherme Moreira <ceschiatti@gmail.com>
2020-08-22 15:12:24 -0300

    Change script after running arandr

--- xrandr.sh
+++ xrandr.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --mode 1680x1050 --pos 0x0 --rotate normal --output eDP-1-1 --primary --mode 1920x1080 --pos 1680x121 --rotate normal --output DP-1-1 --off
+xrandr --output HDMI-0 --mode 1680x1050 --pos 0x136 --rotate normal --output eDP-1-1 --primary --mode 1920x1080 --pos 1680x121 --rotate normal --output DP-1-1 --off
