6caaf0b867503c15541fbff65fd42960c7369acf
Guilherme Moreira <ceschiatti@gmail.com>
2020-10-09 11:32:05 -0300

    Manjaro tweaks.

--- linux.sh
+++ linux.sh
@@ -4,3 +4,8 @@ sudo apt install libgl-dev
 myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 echo "My WAN/Public IP address: ${myip}"
 
+## https://itsfoss.com/things-to-do-after-installing-manjaro/
+yay ttf-ms-fonts
+sudo systemctl enable fstrim.timer
+sudo pacman-mirrors --fasttrack
+
